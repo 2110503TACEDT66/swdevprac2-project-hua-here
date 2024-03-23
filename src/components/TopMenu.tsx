3cb4c7c387748d408c39eb4004a6f2c07898434e
Pacharapol K. <pcr.pol101@gmail.com>
2024-03-23 18:01:36 +0700

    added basic components

---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import TopMenuItem from "./TopMenuItem";
+
+export default async function TopMenu() {
+
+    return (
+        <div className="h-12 bg-gray-50 absolute inset-x-0 top-0 z-30 border-t border-b border-gray-300 flex flex-row">
+            <Image src={'/Images/Logo.png'} className="h-auto w-auto" alt='logo' width={0} height={0} sizes="100vh" />
+            <TopMenuItem title="Reservation" pageRef="/reservations" />
+        </div>
+    )
+}
