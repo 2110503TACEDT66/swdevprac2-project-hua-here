03ef6bed63582c02b482d573d47f64ca7391136b
Pacharapol K. <pcr.pol101@gmail.com>
2024-03-24 17:31:59 +0700

    Added basic Register-Login Function

---
+++ src/app/(Auth)/register/page.tsx
@@ -0,0 +1,57 @@
+export default async function RegisterPage() {
+    const RegisterUser = async (addRegisterForm: FormData) => {
+        'use server'
+        const name = addRegisterForm.get('name');
+        const email = addRegisterForm.get('email');
+        const tel = addRegisterForm.get('tel');
+        const password = addRegisterForm.get('password');
+
+        try {
+            const response = await fetch('http://localhost:5000/api/v1/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    tel: tel,
+                    password: password
+                })
+            });
+
+            if (!response.ok) throw new Error("Registration failed");
+            console.log("Register Success");
+            window.location.href = '/login';
+        } catch (err) {
+            console.log(err);
+        } 
+    }
+
+    return (
+        <form action={RegisterUser}>
+            <div className="text-xl text-blue-700">Register</div>
+            <div className="flex items-center w-1/2 my-2">
+                <label className="w-auto block text-gray-700 pr-4" htmlFor="name">Name</label>
+                <input type="text" required id='name' name="name" placeholder="Enter your name"
+                className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400" />
+            </div>
+            <div className="flex items-center w-1/2 my-2">
+                <label className="w-auto block text-gray-700 pr-4" htmlFor="tel">Telephone</label>
+                <input type="text" required id='tel' name="tel" placeholder="Enter your telephone number"
+                className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400" />
+            </div>
+            <div className="flex items-center w-1/2 my-2">
+                <label className="w-auto block text-gray-700 pr-4" htmlFor="email">Email</label>
+                <input type="text" required id='email' name="email" placeholder="Enter your email"
+                className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400" />
+            </div>
+            <div className="flex items-center w-1/2 my-2">
+                <label className="w-auto block text-gray-700 pr-4" htmlFor="password">Password</label>
+                <input type="password" required id='password' name="password" placeholder="Enter your password"
+                className="bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400" />
+            </div>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">Register</button>
+        </form>
+    )
+}
