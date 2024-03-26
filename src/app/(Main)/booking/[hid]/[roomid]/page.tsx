d6537c428bf78c645009c047a2ab18a25cec59ab
HuaTanakrit <Tanakit.pitak@gmail.com>
2024-03-26 22:26:51 +0700

    Finish

--- src/app/(Main)/booking/[hid]/[roomid]/page.tsx
+++ src/app/(Main)/booking/[hid]/[roomid]/page.tsx
@@ -3,7 +3,7 @@ import CheckIncheckoutDate from "@/components/CheckIncheckOutdate";
 export default async function BookingPage({params} : {params: {hid:string,roomid:string}}) {
 
     return (
-        <main className="left-0 top-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40">
+        <main className="left-0 top-0 w-full h-full flex items-center justify-center bg-[url('/Images/bg.png')]  bg-cover bg-center bg-no-repeat h-screen">
             <CheckIncheckoutDate hid={params.hid} roomid={params.roomid} />
         </main>
     )
