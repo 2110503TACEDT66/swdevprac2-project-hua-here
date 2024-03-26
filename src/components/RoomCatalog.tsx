5c9085979a8b8489f9fa0e806b7853a5d60be084
HuaTanakrit <Tanakit.pitak@gmail.com>
2024-03-26 15:38:29 +0700

    RoomCard

--- src/components/RoomCatalog.tsx
+++ src/components/RoomCatalog.tsx
@@ -1,16 +1,18 @@
 import Card from "./Card";
 import Link from "next/link";
+import RoomCard from "./RoomCard";
+
 
 
 export default async function RoomCatalog({roomJson}:{roomJson:Promise<RoomJson>}){
     const roomJsonReady = await roomJson
     return (
-        <div className="m-5 flex flex-row flex-wrap justify-around content-around p-2.5">
+        <div className="m-5 flex flex-row flex-wrap justify-around content-around p-2.5  w-full">
         {
             roomJsonReady.data.map((RoomItem)=>(
-                <Link href={`/rooms/${RoomItem._id}`} className="w-1/5" key={RoomItem._id}>
-                  <Card hotelName={RoomItem.roomNo} imgSrc={RoomItem.picture} />
-                </Link>
+                <div className="w-4/5 py-5 flex flex-col relative" key={RoomItem._id}> 
+                  <RoomCard RoomItem={RoomItem} />
+                  </div>
             ))
         }
         </div>
