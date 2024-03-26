c1082f36658644657779e008f5d3c77cf89fbfc6
Pacharapol K. <pcr.pol101@gmail.com>
2024-03-26 13:28:42 +0700

    fix catalog and profile

--- src/app/(Main)/page.tsx
+++ src/app/(Main)/page.tsx
@@ -2,13 +2,18 @@ import Image from 'next/image'
 import Banner from '@/components/Banner'
 import HotelCatalog from '@/components/HotelCatalog'
 import getHotels from '@/libs/getHotels'
+import { Suspense } from 'react'
+import LinearProgress from '@mui/material/LinearProgress'
 
 export default function Home() {
   const hotel = getHotels()
   return (
     <main>
       <Banner/>
-      <HotelCatalog hotelJson={hotel}/>
+      <h1 className="text-xl font-medium text-pink-200">Hotels</h1>
+      <Suspense fallback = {<h3 className="text-red-400">Loading...<LinearProgress /></h3>}>
+          <HotelCatalog hotelJson={hotel} />
+      </Suspense>
     </main>
   )
 }
